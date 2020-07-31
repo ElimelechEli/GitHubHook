f77f971cbe2bdbcc516631e1ac2b819375ab44cc
Eli Elimelech <elimelech.eli@gmail.com>
2020-07-31 08:39:47 +0300

    Added initial

---
+++ CB_GitHub/Controllers/HookInterceptController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CB_GitHub.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CB_GitHub.Controllers
+{
+    [ApiController]
+    [Route("/githubhook")]
+    public class HookInterceptController : ControllerBase
+    {
+        private readonly ILogger<HookInterceptController> _logger;
+
+        public HookInterceptController(ILogger<HookInterceptController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public object Post([FromHeader] Dictionary<string, string> header, [FromBody] object body)
+        {
+            return body;
+        }
+    }
+}
